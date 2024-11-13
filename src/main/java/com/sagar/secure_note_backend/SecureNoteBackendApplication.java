b82d54ec8997a8358b67f3c68a82b092483dda6e
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-11-13 20:08:40 +0600

    start over from backend with security

---
+++ src/main/java/com/sagar/secure_note_backend/SecureNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.secure_note_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureNoteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/secure_note_backend/SecureNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.secure_note_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureNoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
