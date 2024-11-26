bcd583e82e2b0291127f9d6a123ae060491f0b23
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-11-27 01:36:03 +0600

    api route protection

--- src/main/java/com/sagar/secure_note_backend/security/SecurityConfig.java
+++ src/main/java/com/sagar/secure_note_backend/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,8 +15,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
-                //.formLogin(withDefaults())
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin").denyAll()
+                        .requestMatchers("/admin/**").denyAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .formLogin(withDefaults());
                 .httpBasic(withDefaults());
         return http.build();
     }
